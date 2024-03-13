325e79f0239e2dffcbdd6766f495dfec6709e8b4
weshh20 <125647459+weshh20@users.noreply.github.com>
2024-03-13 14:25:03 +0000

    mysql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+SELECT score, name * FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+ay my name
+-- don't list rows without name
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
