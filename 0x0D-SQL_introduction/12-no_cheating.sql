f4125fc1fa36d225edc0de7990f222338a80f2db
weshh20 <125647459+weshh20@users.noreply.github.com>
2024-03-13 14:34:24 +0000

    mysql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,3 @@
--- updating bobs score
-UPDATE second_table SET score = 10 WHERE name = Bob
+-- Cheating is bad
+-- update bob's score to 10
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
-ay my name
+-- Say my name
 -- don't list rows without name
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
