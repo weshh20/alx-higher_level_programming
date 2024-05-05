10e1215b657ef19514fbb7f45fd533e2bae2bfe8
weshh20 <125647459+weshh20@users.noreply.github.com>
2024-05-05 08:53:12 -0700

    javascripting

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-// It prints 3 lines
-
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+// it prints three lines
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
